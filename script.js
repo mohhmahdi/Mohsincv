6579970548ff3f816c0b810b5b3f9daa8ccb310a
mohhmahdi <mohsin.mahdi96@gmail.com>
2024-08-26 13:57:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.querySelector('.contact-form button').addEventListener('click', functio
     event.preventDefault(); // Prevent default form submission
     alert('Your message has been sent!');
 });
+document.querySelector('.whatsapp-chat-icon').addEventListener('click', () => {
+  document.querySelector('.whatsapp-chat-container').style.display = 'block';
+});
+
+document.querySelector('.whatsapp-chat-container').addEventListener('click', (e) => {
+  if (e.target.closest('.whatsapp-chat-icon') === null) {
+    document.querySelector('.whatsapp-chat-container').style.display = 'none';
+  }
+});

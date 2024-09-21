e32637068dd8477bb093242d582c5b031f27b4e7
mohhmahdi <mohsin.mahdi96@gmail.com>
2024-09-21 16:59:48 +0300

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
     
-    $to = "_mainaccount@mohsinmahdi.in"; // Replace with your email address
+    $to = "mohsinmahdi@mohsinmahdi.in"; // Replace with your email address
     $subject = "New Message from Contact Form";
     $body = "Name: $name\nEmail: $email\nMessage: $message";
     $headers = "From: $email";
